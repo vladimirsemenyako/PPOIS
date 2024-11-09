6c2179b40fcd4a247ac2b3ae5f253cbbea5d4375
Vladimir Semenyako <2243880@gmail.com>
2024-11-09 10:20:42 +0300

    lab_4 | Final Version

---
+++ sem_1/lab_4/README.md
@@ -0,0 +1,14 @@
+### 📝 I developed an *undirected graph* structure using an *adjacency list*. This involved designing the core functionalities like adding/removing vertices and edges, checking for specific vertices and edges, counting vertices and edges, and calculating vertex degrees.
+
+### 🧪 I used *Google Test (gtest)* to write and run unit tests for my graph classes. The test coverage of my project is here:
+
+![alt text](<img/Screenshot 2024-11-09 at 10.11.46.png>)
+
+### Demonstration of the functionality of this data structure: 
+
+![alt text](<img/Screenshot 2024-11-09 at 10.17.39.png>)
+
+
+
+
+
--- sem_1/lab_4/main.cpp
+++ sem_1/lab_4/main.cpp
@@ -4,84 +4,72 @@
 // In Main.cpp there is example of working this program as a some sort of framework.
 
 int main () {
-    // // Создаём объект графа
     // NonOrientedGraph<int> graph;
     //
-    // // Добавляем вершины
-    // std::cout << "Добавляем вершины 1, 2, 3, 4\n";
+    // std::cout << "Adding Vertices 1, 2, 3, 4\n";
     // graph.addVertex(1);
     // graph.addVertex(2);
     // graph.addVertex(3);
     // graph.addVertex(4);
     //
-    // // Добавляем рёбра
-    // std::cout << "Добавляем рёбра (1, 2), (2, 3), (3, 4), (4, 1)\n";
+    // std::cout << "Adding Edges (1, 2), (2, 3), (3, 4), (4, 1)\n";
     // graph.addEdge(1, 2);
     // graph.addEdge(2, 3);
     // graph.addEdge(3, 4);
     // graph.addEdge(4, 1);
     //
-    // // Проверка наличия вершин и рёбер
-    // std::cout << "Проверка наличия вершины 2: " << graph.checkVertex(2) << "\n";
-    // std::cout << "Проверка наличия вершины 5: " << graph.checkVertex(5) << "\n";
-    // std::cout << "Проверка наличия ребра (1, 2): " << graph.checkEdge(1, 2) << "\n";
-    // std::cout << "Проверка наличия ребра (2, 4): " << graph.checkEdge(2, 4) << "\n";
+    // std::cout << "Checking for the presence of vertex 2: " << graph.checkVertex(2) << "\n";
+    // std::cout << "Checking for the presence of vertex 5: " << graph.checkVertex(5) << "\n";
+    // std::cout << "Checking for the presence of edge (1, 2): " << graph.checkEdge(1, 2) << "\n";
+    // std::cout << "Checking for the presence of edge (2, 4): " << graph.checkEdge(2, 4) << "\n";
     //
-    // // Подсчет вершин и рёбер
-    // std::cout << "Количество вершин в графе: " << graph.vertexCount() << "\n";
-    // std::cout << "Количество рёбер в графе: " << graph.edgeCount() << "\n";
+    // std::cout << "Amount of vertices in graph: " << graph.vertexCount() << "\n";
+    // std::cout << "Amount of edges in graph: " << graph.edgeCount() << "\n";
     //
-    // // Степень вершины
-    // std::cout << "Степень вершины 2: " << graph.vertexDegree(2) << "\n";
+    // std::cout << "Vertex degree 2: " << graph.vertexDegree(2) << "\n";
     //
-    // // Итератор вершин
-    // std::cout << "\nИтерация по вершинам:\n";
+    // std::cout << "\nIteration through vertices:\n";
     // for (auto it = graph.vertexBegin(); it != graph.vertexEnd(); ++it) {
     //     std::cout << *it << " ";
     // }
     // std::cout << "\n";
     //
-    // // Итератор рёбер
-    // std::cout << "\nИтерация по рёбрам:\n";
+    // std::cout << "\nIteration through edges:\n";
     // for (auto it = graph.edgeBegin(); it != graph.edgeEnd(); ++it) {
     //     auto edge = *it;
     //     std::cout << "(" << edge.first << ", " << edge.second << ") ";
     // }
     // std::cout << "\n";
     //
-    // // Инцидентные рёбра для вершины 2
-    // std::cout << "\nИнцидентные рёбра для вершины 2:\n";
+    // std::cout << "\nIncident edges for a vertex 2:\n";
     // for (auto it = graph.edgeIncidentBegin(2); it != graph.edgeIncidentEnd(2); ++it) {
     //     auto edge = *it;
     //     std::cout << "(" << edge.first << ", " << edge.second << ") ";
     // }
     // std::cout << "\n";
     //
-    // // Смежные вершины для вершины 2
-    // std::cout << "\nСмежные вершины для вершины 2:\n";
+    // std::cout << "\nAdjacent vertices for a vertex 2:\n";
     // for (auto it = graph.adjacentBegin(2); it != graph.adjacentEnd(2); ++it) {
     //     std::cout << *it << " ";
     // }
     // std::cout << "\n";
     //
-    // // Удаление вершины и ребра
-    // std::cout << "\nУдаляем вершину 3 и ребро (1, 2)\n";
+    // std::cout << "\nRemoving vertex 3 and edge (1, 2)\n";
     // graph.removeVertex(3);
     // graph.removeEdge(1, 2);
     //
-    // // Итерация по вершинам и рёбрам после удаления
-    // std::cout << "Вершины после удаления:\n";
+    // std::cout << "Vertices after deleting:\n";
     // for (auto it = graph.vertexBegin(); it != graph.vertexEnd(); ++it) {
     //     std::cout << *it << " ";
     // }
     // std::cout << "\n";
     //
-    // std::cout << "Рёбра после удаления:\n";
+    // std::cout << "Edges after deleting:\n";
     // for (auto it = graph.edgeBegin(); it != graph.edgeEnd(); ++it) {
     //     auto edge = *it;
     //     std::cout << "(" << edge.first << ", " << edge.second << ") ";
     // }
     // std::cout << "\n";
-    //
+
     return 0;
 }
